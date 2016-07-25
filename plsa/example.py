0b1754a0fad0f9ce8e2e4f598d8e2f6ea4f9398e
chin <chin.qian@fuwo.com>
2016-07-25 19:08:24 +0800

    plsa中数据读写的准备阶段独立到example中

---
+++ plsa/example.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2016-7-25
+
+@author: chin
+'''
+
+from plsa import PLSA
+import re
+import os
+import pickle
+
+import sys
+reload(sys)
+sys.setdefaultencoding("utf8")
+
+
+word_list = []
+word_dict = {}
+docs = []
+
+def prepare_data_from_dir(train_dir, stopword_set):
+    non_chinese = re.compile(u"[^\u4E00-\u9FA5]")   # 所有非汉字的unicode编码范围
+    for filename in os.listdir(train_dir):
+        with open(os.path.join(train_dir, filename)) as fr:
+            print filename
+            doc = {}
+            words = non_chinese.sub(" ", fr.read().decode("utf-8"))
+            for word in words.strip().split():
+                if word in stopword_set:
+#                     print word
+                    continue
+                if word not in word_list:
+                    word_dict[word] = len(word_list)
+                    word_list.append(word)
+                doc.setdefault(word_dict[word], 0)
+                doc[word_dict[word]] += 1
+            docs.append(doc)
+
+
+def filter_stopwords(filename):
+    stopword_set = set([])
+    with open(filename) as fr:
+        for word in fr:
+            stopword_set.add(word.strip().decode("utf-8"))
+    return stopword_set
+
+
+def result_save(plsa_method, result_path="result/new"):
+    with open(os.path.join(result_path, "doc_topic.txt"), "w") as fw:
+        for row in plsa_method.pdoc_topic:
+            for prob in row:
+                fw.write("%f\t" % prob)
+            fw.write("\n")
+    with open(os.path.join(result_path, "topic_word.txt"), "w") as fw:
+        for row_id, row in enumerate(plsa_method.ptopic_word):
+            fw.write("class: %d\n" % row_id)
+            topic_word_prob = dict(zip(word_list, row))
+            for word, prob in sorted(topic_word_prob.iteritems(), key=lambda item: item[1], reverse=True)[:20]:
+                fw.write(u"\t%s\t%f\n" % (word, prob))
+            fw.write(os.linesep)
+
+
+def main():
+#     prepare_data_from_dir(u"E:\\07-Repository\\语料\\data-分类", filter_stopwords("stopwords.txt"))
+#     pickle.dump(docs, file("result/new/docs.list", "w"))
+#     pickle.dump(word_list, file("result/new/words.list", "w"))
+    global docs
+    global word_list
+    docs = pickle.load(file("result/new/docs.list"))
+    word_list = pickle.load(file("result/new/words.list"))
+    plsa_method = PLSA(docs=docs)
+    plsa_method.training_em(topic_num=3, iter_num=100, epsilon=1e-6)
+    result_save(plsa_method)
+
+
+if __name__ == "__main__":
+    main()
--- plsa/plsa.py
+++ plsa/plsa.py
@@ -11,17 +11,24 @@ import numpy as np
 from sklearn import preprocessing
 import math
 
+
 class PLSA(object):
     
-    def __init__(self):
-#         self.pdoc = None
+    def __init__(self, docs):
         self.pdoc_topic = None
         self.ptopic_word = None
 
-        self.word_dict = {}
-        self.words = []
-        self.docs = []
+        self.word_set = []
+        self.docs = docs
 
+        self._initial_wordset()
+
+
+    def _initial_wordset(self):
+        for doc in self.docs:
+            for word in doc:
+                self.word_set.append(word)
+    
 
     def training_em(self, topic_num=3, iter_num=100, epsilon=1e-6, train_data="data/train.dat"):
         '''
@@ -30,9 +37,10 @@ class PLSA(object):
         @param epsilon: 误差上限，当前后两次的Q函数值小于epsilon时会停止迭代
         @param train_data: 训练数据的位置
         '''
-        self.topic_num = topic_num
-        self.prepare_data(train_data)
-        self.random_initial_value()
+#         self.topic_num = topic_num
+#         self.prepare_data(train_data)
+#         self.prepare_data_from_dir(train_data)
+        self.random_initial_probabity(topic_num)
         
         # 临时的p(topic|doc, word), 为了防止维护的临时矩阵过大，下面会对每一个doc轮流处理        
         old_Qvalue = 0.0
@@ -47,7 +55,7 @@ class PLSA(object):
                 for wd_id, wd_cnt in doc.iteritems():
                     tmp_ptopic_given_docwd = self.pdoc_topic[doc_id] * self.ptopic_word[:, wd_id]
                     tmp_ptopic_given_docwd = tmp_ptopic_given_docwd / float(sum(tmp_ptopic_given_docwd))                        
-                    for topic_id in xrange(self.topic_num):
+                    for topic_id in xrange(topic_num):
                         tmp_pdoc_topic[doc_id][topic_id] += wd_cnt * tmp_ptopic_given_docwd[topic_id]
                         tmp_ptopic_word[topic_id][wd_id] += wd_cnt * tmp_ptopic_given_docwd[topic_id]
 
@@ -65,44 +73,7 @@ class PLSA(object):
 #         print self.pdoc_topic
 
 
-    def prepare_data(self, train_data):
-        for line in file(train_data):
-            doc = {}
-            words = line.strip().split()
-            for word in words:
-                if word not in self.words:
-                    self.word_dict[word] = len(self.words)
-                    self.words.append(word)
-                doc.setdefault(self.word_dict[word], 0)
-                doc[self.word_dict[word]] += 1
-            self.docs.append(doc)
+    def random_initial_probabity(self, topic_num):
+        self.pdoc_topic = preprocessing.normalize(np.random.random((len(self.docs), topic_num)), norm="l1", axis=1)
+        self.ptopic_word = preprocessing.normalize(np.random.random((topic_num, len(self.word_set))), norm="l1", axis=1)        
 
-
-    def random_initial_value(self):
-        self.pdoc_topic = preprocessing.normalize(np.random.random((len(self.docs), self.topic_num)), norm="l1", axis=1)
-        self.ptopic_word = preprocessing.normalize(np.random.random((self.topic_num, len(self.word_dict))), norm="l1", axis=1)
-
-
-    def result_save(self, result_path):
-        with open(os.path.join(result_path, "doc_topic.txt"), "w") as fw:
-            for row in self.pdoc_topic:
-                for prob in row:
-                    fw.write("%f\t" % prob)
-                fw.write("\n")
-        with open(os.path.join(result_path, "topic_word.txt"), "w") as fw:
-            for row_id, row in enumerate(self.ptopic_word):
-                fw.write("class: %d\n" % row_id)
-                topic_word_prob = dict(zip(self.words, row))
-                for word, prob in sorted(topic_word_prob.iteritems(), key=lambda item: item[1], reverse=True)[:20]:
-                    fw.write("\t%s\t%f\n" % (word, prob))
-                fw.write(os.linesep)
-
-
-if __name__ == "__main__":
-    plsa_method = PLSA()
-    plsa_method.training_em(topic_num=3, iter_num=100, train_data="data/train.dat")
-
-#     plsa_method.result_save(result_path="data/")
-
-    print len(plsa_method.words)
-    print plsa_method.pdoc_topic
